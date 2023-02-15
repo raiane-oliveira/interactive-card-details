d8484a02a735db31f22dd2689afab95f922fd51c
Raiane <raiane.oliveira404@gmail.com>
2023-02-15 14:33:47 -0300

    feat: mensagens de erro se os campos estiverem vazios

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
+// Credit card fields
 const cardFrontNumber = document.querySelector("#card-front-number")
 const cardFrontName = document.querySelector("#card-front-name")
 const cardFrontDate = document.querySelector("#card-front-date")
 const cardBackCVC = document.querySelector("#card-back-cvc")
-const formCard = document.querySelector(".form-card")
 
-let month = 00, year = 00
+// Form fields
+const formCard = document.querySelector(".form-card")
+const formCardName = document.querySelector(".card-name")
+const formCardNumber = document.querySelector(".card-number")
+const formCardDates = document.querySelectorAll(".card-exp-date")
+const formCardCVC = document.querySelector(".card-cvc")
+
+let month = "00", year = "00"
 
 formCard.addEventListener("keyup", (event) => {
+    clearErrorMessages()
     const element = event.target
 
-    let isCardNameInput = element.className.includes("card-name") && element.value
-    let isCardNumberInput = element.className.includes("card-number") && element.value
-    let isCardDateInput = element.className.includes("card-exp-date") && element.value
-    let isCardCVCInput = element.className.includes("card-cvc") && element.value
+    let isCardNameInput = element.className.includes("card-name")
+    let isCardNumberInput = element.className.includes("card-number")
+    let isCardDateInput = element.className.includes("card-exp-date")
+    let isCardCVCInput = element.className.includes("card-cvc")
 
     if (isCardNameInput) {
-        cardFrontName.innerHTML = element.value
+        cardFrontName.innerHTML = formCardName.value ? element.value : "Jane Appleseed"
     }
     
     if (isCardNumberInput) {
-        cardFrontNumber.innerHTML = element.value
+        cardFrontNumber.innerHTML = formCardNumber.value ? element.value : "0000 0000 0000 0000"
     }
     
     if (isCardDateInput) {
@@ -28,11 +36,54 @@ formCard.addEventListener("keyup", (event) => {
         } else if (element.dataset.date === "yy") {
             year = element.value
         }
+
         cardFrontDate.innerHTML = `${month}/${year}`
     }
 
     if (isCardCVCInput) {
-        cardBackCVC.innerHTML = element.value
+        cardBackCVC.innerHTML = formCardCVC.value ? element.value : "000"
     }
-    
 })
+
+formCard.addEventListener("submit", (event) => {
+    event.preventDefault()
+    validateEntriesCard()
+})
+
+function validateEntriesCard() {
+    if (!formCardName.value) {
+        formCardName.style.border = "1px solid hsl(0, 100%, 66%)"
+        document.querySelector(".card-name-container .error-message").style.display = "block"
+        document.querySelector(".card-name-container .error-message").innerHTML = "Can't be blank"
+    }
+
+    if (!formCardNumber.value) {
+        formCardNumber.style.border = "1px solid hsl(0, 100%, 66%)"
+        document.querySelector(".card-number-container .error-message").style.display = "block"
+        document.querySelector(".card-number-container .error-message").innerHTML = "Can't be blank"
+    }
+
+    if (!formCardCVC.value) {
+        formCardCVC.style.border = "1px solid hsl(0, 100%, 66%)"
+        document.querySelector(".card-cvc-container .error-message").style.display = "block"
+        document.querySelector(".card-cvc-container .error-message").innerHTML = "Can't be blank"
+    }
+
+    formCardDates.forEach(date => {
+        if (!date.value) {
+            date.style.border = "1px solid hsl(0, 100%, 66%)"
+            document.querySelector(".card-exp-date + .error-message").style.display = "block"
+            document.querySelector(".card-exp-date + .error-message").innerHTML = "Can't be blank" 
+        }
+    })
+}
+
+function clearErrorMessages() {
+    document.querySelectorAll(".error-message").forEach(error => {
+        error.style.display = "none"
+    })
+
+    document.querySelectorAll("input").forEach(input => {
+        input.style.removeProperty("border")
+    })
+}
